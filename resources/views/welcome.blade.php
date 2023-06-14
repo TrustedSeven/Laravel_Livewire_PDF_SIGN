08fb97386044af7a96107bdf9e32f608aae4b5e0
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-06-14 09:11:10 -0500

    js files added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,20 @@
 
     <title>Laravel</title>
     <!-- Scripts -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.min.js'></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/interact.js/1.2.8/interact.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="../js/app.js"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
 </head>
 
 <body class="antialiased">
-    <div>
+    <h2 class="mt-3">Signature Track</h2>
+    <div class="container">
         @livewire('category')
     </div>
 </body>
