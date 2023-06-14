c69d84da683f8023b32e2683e93947db6c59696f
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-06-14 01:25:22 -0500

    bootstrap enable

--- resources/views/livewire/category.blade.php
+++ resources/views/livewire/category.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            AAA
+        </div>
+        <div class="col-md-9">
+            BBB
+        </div>
+    </div>
 </div>
